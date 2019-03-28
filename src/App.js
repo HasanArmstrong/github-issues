a8670ffbf39caf772eae881d5dc03ed0a574ad19
Hasan Armstrong <hasan_armstrong@hotmail.co.uk>
2019-03-28 15:26:12 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+const clientId = process.env.REACT_APP_CLIENT_ID;
+
+class App extends Component {
+
+constructor() {
+  super();
+  const existingToken = sessionStorage.getItem('token');
+  const accessToken = (window.location.search.split("=")[0] === "?access_token") ? window.location.search.split("=")[1] : null;
+
+  if (!accessToken && !existingToken) {
+    window.location.replace(`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`)
+  }
+
+  if (accessToken) {
+    console.log(`New accessToken: ${accessToken}`);
+
+    sessionStorage.setItem("token", accessToken);
+    this.state = {
+        token: accessToken
+    }
+  }
+
+  if (existingToken) {
+    this.state = {
+      token: existingToken
+    };
+  }    
+}
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
+}
+
+export default App;
