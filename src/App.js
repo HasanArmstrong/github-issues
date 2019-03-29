964a45a7a016085e63005b64a7eec57c3c0b3c82
lvltcode <lvltcode@gmail.com>
2019-03-29 21:29:18 +0700

    add full page

--- src/App.js
+++ src/App.js
@@ -1,60 +1,111 @@
-import React, { Component } from "react";
+
+import React, { Component } from 'react';
+import './App.css';
+import PaginationComponent from "react-reactstrap-pagination";
+import "bootstrap/dist/css/bootstrap.min.css";
 import Issue from "./Issue";
-import "./App.css";
+import SearchContainer from './SearchContainer';
+
+
 const clientId = process.env.REACT_APP_CLIENT_ID;
 
 class App extends Component {
   constructor() {
     super();
     const state = {};
-    const existingToken = sessionStorage.getItem("token");
-    const accessToken =
-      window.location.search.split("=")[0] === "?access_token"
-        ? window.location.search.split("=")[1]
-        : null;
+    this.state = {
+      selectedPage: 1,
 
+    };
+  
+    this.handleSelected = this.handleSelected.bind(this);
+  
+
+    const existingToken = sessionStorage.getItem('token');
+    const accessToken = (window.location.search.split("=")[0] === "?access_token") ? window.location.search.split("=")[1] : null;
     if (!accessToken && !existingToken) {
-      window.location.replace(
-        `https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`
-      );
+      window.location.replace(`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`)
     }
-
+  
     if (accessToken) {
       console.log(`New accessToken: ${accessToken}`);
-
+  
       sessionStorage.setItem("token", accessToken);
-      state.token = accessToken;
       this.state = {
-        token: accessToken
-      };
+          token: accessToken
+      }
     }
-
+  
     if (existingToken) {
-      state.token = existingToken;
-    }
-    this.state = {
+      this.state = {
+        token: existingToken
+      };
+    }    
+  
+
+  this.state = {
       issues: [],
       token: state.token
     };
-  }
+  
+}
+
+
+
+handleSearch(owner, repo) {
+  //use for searchContainer,ex: update 'facebook/react' to state.value
+  console.log(owner, repo)
+  this.setState({value: `${owner}/${repo}`}, () => console.log(this.state))
+}
+
 
   async componentDidMount() {
     const url =
-      "https://api.github.com/repos/HasanArmstrong/github-issues/issues";
+      "https://api.github.com/repos/AdeleD/react-paginate/issues?page=1&per_page=10"
+      console.log(this.state.issues)
     let resp = await fetch(url);
     let json = await resp.json();
     this.setState({
       issues: json
     });
+    
   }
 
+async getIsusses(arg){
+  const url =
+  `https://api.github.com/repos/AdeleD/react-paginate/issues?page=${arg}&per_page=10`
+  console.log(this.state.issues)
+let resp = await fetch(url);
+let json = await resp.json();
+this.setState({
+  issues: json
+});
+}
+
+handleSelected(selectedPage) {
+  console.log("selected", selectedPage);
+  this.setState({ selectedPage: selectedPage });
+  this.getIsusses(selectedPage)
+}
+
   render() {
+    
     return (
-      <div className="App">
-        <Issue issueList={this.state.issues} />
+      <div className="App ">
+      <div className="justify-content-center">
+      <div className="d-flex justify-content-center">
+       <PaginationComponent totalItems={10} pageSize={2} onSelect={this.handleSelected} />
+       </div>
+       <div className="d-flex justify-content-center">
+       <SearchContainer handleSearch={(owner, repo) => this.handleSearch(owner, repo)}/>
+       </div>
+       <Issue issueList={this.state.issues} />
+       </div>
+
       </div>
     );
   }
 }
+
 
 export default App;
