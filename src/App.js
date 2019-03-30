d990cb06207edf749e25b0d9883620b8fc736542
lvltcode <lvltcode@gmail.com>
2019-03-30 16:52:05 +0700

    90%

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import PaginationComponent from "react-reactstrap-pagination";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Issue from "./Issue";
-import SearchContainer from './SearchContainer';
-
+import SearchContainer from "./SearchContainer";
 
 const clientId = process.env.REACT_APP_CLIENT_ID;
 
@@ -14,98 +12,97 @@ class App extends Component {
     super();
     const state = {};
     this.state = {
-      selectedPage: 1,
-
+      selectedPage: 1
     };
-  
-    this.handleSelected = this.handleSelected.bind(this);
-  
 
-    const existingToken = sessionStorage.getItem('token');
-    const accessToken = (window.location.search.split("=")[0] === "?access_token") ? window.location.search.split("=")[1] : null;
+    this.handleSelected = this.handleSelected.bind(this);
+
+    const existingToken = sessionStorage.getItem("token");
+    const accessToken =
+      window.location.search.split("=")[0] === "?access_token"
+        ? window.location.search.split("=")[1]
+        : null;
     if (!accessToken && !existingToken) {
-      window.location.replace(`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`)
+      window.location.replace(
+        `https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`
+      );
     }
-  
+
     if (accessToken) {
       console.log(`New accessToken: ${accessToken}`);
-  
+
       sessionStorage.setItem("token", accessToken);
       this.state = {
-          token: accessToken
-      }
+        token: accessToken
+      };
     }
-  
+
     if (existingToken) {
       this.state = {
         token: existingToken
       };
-    }    
-  
+    }
 
-  this.state = {
+    this.state = {
       issues: [],
       token: state.token
     };
-  
-}
+  }
 
-
-
-handleSearch(owner, repo) {
-  //use for searchContainer,ex: update 'facebook/react' to state.value
-  console.log(owner, repo)
-  this.setState({value: `${owner}/${repo}`}, () => console.log(this.state))
-}
-
+  handleSearch(owner, repo) {
+    //use for searchContainer,ex: update 'facebook/react' to state.value
+    console.log(owner, repo);
+    this.setState({ value: `${owner}/${repo}` }, () => console.log(this.state));
+  }
 
   async componentDidMount() {
     const url =
-      "https://api.github.com/repos/AdeleD/react-paginate/issues?page=1&per_page=10"
-      console.log(this.state.issues)
+      "https://api.github.com/repos/facebook/react/issues?page=1&per_page=10";
+    console.log(this.state.issues);
     let resp = await fetch(url);
     let json = await resp.json();
     this.setState({
       issues: json
     });
-    
   }
 
-async getIsusses(arg){
-  const url =
-  `https://api.github.com/repos/AdeleD/react-paginate/issues?page=${arg}&per_page=10`
-  console.log(this.state.issues)
-let resp = await fetch(url);
-let json = await resp.json();
-this.setState({
-  issues: json
-});
-}
+  async getIssues(arg) {
+    const url = `https://api.github.com/repos/facebook/react/issues?page=${arg}&per_page=10`;
+    console.log(this.state.issues);
+    let resp = await fetch(url);
+    let json = await resp.json();
+    this.setState({
+      issues: json
+    });
+  }
 
-handleSelected(selectedPage) {
-  console.log("selected", selectedPage);
-  this.setState({ selectedPage: selectedPage });
-  this.getIsusses(selectedPage)
-}
+  handleSelected(selectedPage) {
+    console.log("selected", selectedPage);
+    this.setState({ selectedPage: selectedPage });
+    this.getIssues(selectedPage);
+  }
 
   render() {
-    
     return (
       <div className="App ">
-      <div className="justify-content-center">
-      <div className="d-flex justify-content-center">
-       <PaginationComponent totalItems={10} pageSize={2} onSelect={this.handleSelected} />
-       </div>
-       <div className="d-flex justify-content-center">
-       <SearchContainer handleSearch={(owner, repo) => this.handleSearch(owner, repo)}/>
-       </div>
-       <Issue issueList={this.state.issues} />
-       </div>
-
+        <div className="justify-content-center">
+          <div className="d-flex justify-content-center">
+            <PaginationComponent
+              totalItems={10}
+              pageSize={2}
+              onSelect={this.handleSelected}
+            />
+          </div>
+          <div className="d-flex justify-content-center">
+            <SearchContainer
+              handleSearch={(owner, repo) => this.handleSearch(owner, repo)}
+            />
+          </div>
+          <Issue issueList={this.state.issues} />
+        </div>
       </div>
     );
   }
 }
-
 
 export default App;
--- src/Issue.js
+++ src/Issue.js
@@ -1,40 +1,99 @@
 import React, { Component } from "react";
-import { Media } from "reactstrap";
+import {
+  Media,
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem
+} from "reactstrap";
 import "./Issue.css";
 import moment from "moment";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faExclamationCircle } from "@fortawesome/free-solid-svg-icons";
+
+const ReactMarkdown = require("react-markdown");
 
 export default class Issue extends Component {
   render() {
-    console.log(this.props.issueList);
     return (
       <div>
         {this.props.issueList.map(issue => (
-          <Media>
-            <Media body className="mb-3">
-              <Media heading className="d-flex justify-content-start mb-0 ml-4">
-                <div href="#">
-                  <a href={issue.html_url}>#</a> {issue.number} {issue.title}
+          <div>
+            {/* <Navbar color="light" light expand="md" className="mx-4 mb-2">
+              <NavbarBrand href="/">Open</NavbarBrand>
+              <NavbarToggler onClick={this.toggle} />
+              <Collapse navbar>
+                <Nav className="ml-auto" navbar>
+                  <NavItem>
+                    <NavLink href="/components/">Author</NavLink>
+                  </NavItem>
+                  <NavItem>
+                    <NavLink href="https://github.com/reactstrap/reactstrap">
+                      Projects
+                    </NavLink>
+                  </NavItem>
+                  <UncontrolledDropdown nav inNavbar>
+                    <DropdownToggle nav caret>
+                      Labels
+                    </DropdownToggle>
+                    <DropdownMenu right>
+                      <DropdownItem>st</DropdownItem>
+                      <DropdownItem>st2</DropdownItem>
+                      <DropdownItem divider />
+                      <DropdownItem>Reset</DropdownItem>
+                    </DropdownMenu>
+                  </UncontrolledDropdown>
+                </Nav>
+              </Collapse>
+            </Navbar> */}
+            <Media className="Is mx-2">
+              <Media className="col-1 openColor d-flex justify-content-end small pt-1">
+                <div className="mr-1">
+                  <FontAwesomeIcon icon={faExclamationCircle} />
                 </div>
+                <div>{issue.state}</div>
               </Media>
-              <Media>
-                <div className="small ml-5 mt-0 text-dark">
-                  {moment()
-                    .startOf("day")
-                    .fromNow()}
-                </div>
+              <Media body className="mb-3">
+                <Media heading className="d-flex justify-content-start mb-0">
+                  <div href="#">{issue.title}</div>
+                </Media>
+                <Media>
+                  <div className="small my-0 text-dark px-1">
+                    <p>
+                      <a href={issue.html_url}>#</a>
+                      {issue.number} opened on{" "}
+                      {moment()
+                        .startOf("day")
+                        .fromNow()}
+                    </p>
+                  </div>
+                </Media>
+                <ReactMarkdown
+                  source={issue.body}
+                  className="row ml-2 text-left pl-1 mr-1"
+                />
               </Media>
-              <div className="row ml-5 text-left">{issue.body}</div>
+              <Media left className="col-1">
+                <img
+                  className="from-avatar"
+                  src={issue.user.avatar_url}
+                  width="60"
+                  height="60"
+                />
+                <p className="small text-dark">@{issue.user.login}</p>
+                <span style={{ backgroundColor: issue.labels.color }}>
+                  {issue.labels.name}
+                </span>
+              </Media>
             </Media>
-            <Media left href="#" className="col-2">
-              <img
-                class="from-avatar"
-                src={issue.user.avatar_url}
-                width="60"
-                height="60"
-              />
-              <p className="small text-dark">@{issue.user.login}</p>
-            </Media>
-          </Media>
+          </div>
         ))}
       </div>
     );
