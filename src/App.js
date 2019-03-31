13417d5b8b0f220490b6a5339aa0b64466d69824
Huong Trinh <trtrhuong@gmail.com>
2019-03-31 18:52:25 +0700

    testing selectedPage haven't fixed yet

--- src/App.js
+++ src/App.js
@@ -1,122 +1,129 @@
-
-import React, { Component } from 'react';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
 import PaginationComponent from "react-reactstrap-pagination";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Issue from "./Issue";
-import SearchContainer from './SearchContainer';
-
+import SearchContainer from "./SearchContainer";
 
 const clientId = process.env.REACT_APP_CLIENT_ID;
 
 class App extends Component {
   constructor() {
     super();
+
     
-    this.state = {
-      selectedPage: 1,
-
-    };
-  
     this.handleSelected = this.handleSelected.bind(this);
-  
 
-    const existingToken = sessionStorage.getItem('token');
-    const accessToken = (window.location.search.split("=")[0] === "?access_token") ? window.location.search.split("=")[1] : null;
+    const existingToken = sessionStorage.getItem("token");
+    const accessToken =
+      window.location.search.split("=")[0] === "?access_token"
+        ? window.location.search.split("=")[1]
+        : null;
     if (!accessToken && !existingToken) {
-      window.location.replace(`https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`)
+      window.location.replace(
+        `https://github.com/login/oauth/authorize?scope=user:email,repo&client_id=${clientId}`
+      );
     }
-  
+
     if (accessToken) {
       console.log(`New accessToken: ${accessToken}`);
-  
+
       sessionStorage.setItem("token", accessToken);
       this.state = {
-          token: accessToken
-      }
+        token: accessToken
+      };
     }
-  
+
     if (existingToken) {
       this.state = {
         token: existingToken
       };
-    }    
-  
-
-  
-}
-
-
- handleSearch(owner, repo) {
-  //use for searchContainer,ex: update 'facebook/react' to state.value
-  console.log(owner, repo)
-  this.setState({value: `${owner}/${repo}`}, () => this.getIssues(1))
-}
-
-async getIssues(arg) {
-  try{
-    const url =
-    `https://api.github.com/repos/${this.state.value}/issues?page=${arg}&per_page=10&access_token=${this.state.token}`;
-    let resp = await fetch(url);
-    let json = await resp.json();
-    json.message ? 
-    this.setState({issues: null, message: json.message}) :
-    this.setState({ issues: json })
-  } catch(err) {
-    console.log(err)
+    }
   }
- 
-}
+
+  handleSearch(owner, repo) {
+    //use for searchContainer,ex: update 'facebook/react' to state.value
+    console.log(owner, repo);
+    this.setState({ value: `${owner}/${repo}`, selectedPage: 1 }, () =>
+      this.getIssues(1)
+    );
+  }
+
+  async getIssues(arg) {
+    try {
+      const url = `https://api.github.com/repos/${
+        this.state.value
+      }/issues?page=${arg}&per_page=10&access_token=${this.state.token}`;
+      let resp = await fetch(url);
+      let json = await resp.json();
+      json.message
+        ? this.setState({ issues: null, message: json.message })
+        : this.setState({ issues: json }, () => console.log(this.state));
+    } catch (err) {
+      console.log(err);
+    }
+  }
 
   async componentDidMount() {
-    const url =
-      `https://api.github.com/repos/AdeleD/react-paginate/issues?page=1&per_page=10&access_token=${this.state.token}`
-      console.log(this.state.issues)
+    const url = `https://api.github.com/repos/AdeleD/react-paginate/issues?page=1&per_page=10&access_token=${
+      this.state.token
+    }`;
+    console.log(this.state.issues);
     let resp = await fetch(url);
     let json = await resp.json();
     this.setState({
       issues: json,
-      value : 'AdeleD/react-paginate'
-    });   
+      value: "AdeleD/react-paginate"
+    });
   }
 
-// async getIsusses(arg){
-//   const url =
-//   `https://api.github.com/repos/AdeleD/react-paginate/issues?page=${arg}&per_page=10`
-//   console.log(this.state.issues)
-// let resp = await fetch(url);
-// let json = await resp.json();
-// this.setState({
-//   issues: json
-// });
-// }
+  // async getIsusses(arg){
+  //   const url =
+  //   `https://api.github.com/repos/AdeleD/react-paginate/issues?page=${arg}&per_page=10`
+  //   console.log(this.state.issues)
+  // let resp = await fetch(url);
+  // let json = await resp.json();
+  // this.setState({
+  //   issues: json
+  // });
+  // }
 
-handleSelected(selectedPage) {
-  console.log("selected", selectedPage);
-  // this.setState({ selectedPage: selectedPage });
-  this.getIssues(selectedPage)
-}
+  handleSelected(selectedPage) {
+    //for the pagination
+    console.log("selected", selectedPage);
+    this.setState({ selectedPage: selectedPage }, () =>
+      this.getIssues(selectedPage)
+    );
+  }
 
   render() {
-    console.log('state', this.state)
+    console.log("state", this.state);
     return (
       <div className="App ">
-      <div className="justify-content-center">
-      <div className="d-flex justify-content-center">
-       <PaginationComponent totalItems={10} pageSize={2} onSelect={this.handleSelected} />
-       </div>
-       <div className="d-flex justify-content-center">
-       <SearchContainer handleSearch={(owner, repo) => this.handleSearch(owner, repo)}/>
-       {this.state.issues ?
-        <Issue issueList={this.state.issues} /> :
-        <h2 className="m-5">{this.state.message}</h2>
-         }
-       </div>
-      </div>
+        <div className="justify-content-center">
+          <div className="d-flex justify-content-center">
+            <PaginationComponent
+            className="pagination"
+              totalItems={10}
+              pageSize={2}
+              activePage ={this.state.selectedPage}
+              onSelect={e => this.handleSelected(e)}
+            />
+          </div>
+          <div className="d-flex justify-content-center">
+            <SearchContainer
+              handleSearch={(owner, repo) => this.handleSearch(owner, repo)}
+            />
+            {this.state.issues ? (
+              <Issue issueList={this.state.issues} />
+            ) : (
+              <h2 className="m-5">{this.state.message}</h2>
+            )}
+          </div>
+        </div>
       </div>
     );
   }
 }
-
 
 export default App;
