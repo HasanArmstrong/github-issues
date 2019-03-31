7e5197862d39644c6809d82192decadb20fa7989
lvltcode <lvltcode@gmail.com>
2019-03-31 16:58:36 +0700

    fix color labels

--- src/Issue.js
+++ src/Issue.js
@@ -1,18 +1,5 @@
 import React, { Component } from "react";
-import {
-  Media,
-  Collapse,
-  Navbar,
-  NavbarToggler,
-  NavbarBrand,
-  Nav,
-  NavItem,
-  NavLink,
-  UncontrolledDropdown,
-  DropdownToggle,
-  DropdownMenu,
-  DropdownItem
-} from "reactstrap";
+import { Media } from "reactstrap";
 import "./Issue.css";
 import moment from "moment";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
@@ -53,21 +40,21 @@ export default class Issue extends Component {
                 </Nav>
               </Collapse>
             </Navbar> */}
-            <Media className="Is mx-2">
+            <Media className="mx-2">
               <Media className="col-1 openColor d-flex justify-content-end small pt-1">
                 <div className="mr-1">
                   <FontAwesomeIcon icon={faExclamationCircle} />
                 </div>
                 <div>{issue.state}</div>
               </Media>
-              <Media body className="mb-3">
+              <Media body className="Is mb-3">
                 <Media heading className="d-flex justify-content-start mb-0">
                   <div href="#">{issue.title}</div>
                 </Media>
                 <Media>
                   <div className="small my-0 text-dark px-1">
                     <p>
-                      <a href={issue.html_url}>#</a>
+                      <a href={issue.html_url}># </a>
                       {issue.number} opened on{" "}
                       {moment()
                         .startOf("day")
@@ -80,7 +67,7 @@ export default class Issue extends Component {
                   className="row ml-2 text-left pl-1 mr-1"
                 />
               </Media>
-              <Media left className="col-1">
+              <Media left className="col-2" href={issue.user.url}>
                 <img
                   className="from-avatar"
                   src={issue.user.avatar_url}
@@ -88,9 +75,18 @@ export default class Issue extends Component {
                   height="60"
                 />
                 <p className="small text-dark">@{issue.user.login}</p>
-                <span style={{ backgroundColor: issue.labels.color }}>
-                  {issue.labels.name}
-                </span>
+                {issue.labels.length === 0 ? (
+                  <span />
+                ) : (
+                  <span
+                    className="label"
+                    style={{
+                      backgroundColor: "#" + issue.labels[0].color
+                    }}
+                  >
+                    {issue.labels[0].name}
+                  </span>
+                )}
               </Media>
             </Media>
           </div>
